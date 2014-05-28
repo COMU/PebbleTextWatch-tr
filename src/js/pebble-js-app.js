35cbbf0249098fd07674c1dfb01adfc46f9da879
GulsahKose <gulsah.1004@gmail.com>
2014-05-28 18:16:13 +0300

    Changed js file for invert color

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,44 +1,39 @@
-//  file: pebble-js-app.js
-//  auth: Matthew Clark, SetPebble
+var mConfig = {};
 
-// change this token for your project
-var setPebbleToken = 'C45W';
+Pebble.addEventListener("ready", function(e) {
+  loadLocalData();
+  returnConfigToPebble();
+});
 
+Pebble.addEventListener("showConfiguration", function(e) {
+	Pebble.openURL(mConfig.configureUrl);
+});
 
-Pebble.addEventListener('ready', function(e) {
-});
-Pebble.addEventListener('appmessage', function(e) {
-  key = e.payload.action;
-  if (typeof(key) != 'undefined') {
-    var settings = localStorage.getItem(setPebbleToken);
-    if (typeof(settings) == 'string') {
-      try {
-        Pebble.sendAppMessage(JSON.parse(settings));
-      } catch (e) {
-      }
-    }
-    var request = new XMLHttpRequest();
-    request.open('GET', 'http://x.SetPebble.com/api/' + setPebbleToken + '/' + Pebble.getAccountToken(), true);
-    request.onload = function(e) {
-      if (request.readyState == 4)
-        if (request.status == 200)
-          try {
-            Pebble.sendAppMessage(JSON.parse(request.responseText));
-          } catch (e) {
-          }
-    }
-    request.send(null);
-  }
-});
-Pebble.addEventListener('showConfiguration', function(e) {
-  Pebble.openURL('http://x.SetPebble.com/' + setPebbleToken + '/' + Pebble.getAccountToken());
-});
-Pebble.addEventListener('webviewclosed', function(e) {
-  if ((typeof(e.response) == 'string') && (e.response.length > 0)) {
-    try {
-      Pebble.sendAppMessage(JSON.parse(e.response));
-      localStorage.setItem(setPebbleToken, e.response);
-    } catch(e) {
+Pebble.addEventListener("webviewclosed",
+  function(e) {
+    if (e.response) {
+      var config = JSON.parse(e.response);
+      saveLocalData(config);
+      returnConfigToPebble();
     }
   }
-});
+);
+
+function saveLocalData(config) {
+  
+  localStorage.setItem("invert", parseInt(config.invert)); 
+  loadLocalData();
+}
+function loadLocalData() {
+	mConfig.invert = parseInt(localStorage.getItem("invert"));
+	mConfig.configureUrl = "http://www.pebbletr.com/settings/index.html";
+
+	if(isNaN(mConfig.invert)) {
+		mConfig.invert = 0;
+	}
+}
+function returnConfigToPebble() {
+  Pebble.sendAppMessage({
+    "invert":parseInt(mConfig.invert), 
+  });    
+}
